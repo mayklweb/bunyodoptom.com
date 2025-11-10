87eaa103442722369561abe791d63ec8b34daa87
mayklweb <muhammadevvv@gmail.com>
2025-11-10 12:20:48 +0500

    update

--- src/app/section/about/index.jsx
+++ src/app/section/about/index.jsx
@@ -40,8 +40,8 @@ function About() {
           <div className="flex justify-between items-start flex-col lg:flex-row gap-5">
             <h1 className="text-[#002bba] text-2xl font-semibold">О НАС</h1>
             <div className="w-full lg:w-1/2">
-              <h3 className="text-[#121212] text-xl lg:text-3xl text-justify leading-[130%] font-semibold tracking-tight">
-                <span className="font-bold">BUNYOD OPTOM</span> —
+              <h3 className="text-[#121212] text-xl lg:text-3xl text-justify leading-[130%] font-normal  tracking-tight">
+                <span className="font-semibold">BUNYOD OPTOM</span> —
                 это сервис, который помогает предпринимателям покупать товары
                 оптом быстро, удобно и выгодно. Мы соединяем бизнес с
                 проверенными поставщиками и делаем процесс закупки максимально
